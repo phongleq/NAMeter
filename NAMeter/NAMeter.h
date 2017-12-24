3d34088e1fe2361368edffef32e777dbd1f07650
Phong Le <phong.le@wgu.edu>
2017-12-24 13:05:03 -0700

    Initial

---
+++ NAMeter/NAMeter.h
@@ -0,0 +1,19 @@
+//
+//  NAMeter.h
+//  NAMeter
+//
+//  Created by Phong Le on 12/24/17.
+//  Copyright © 2017 Phong Le. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NAMeter.
+FOUNDATION_EXPORT double NAMeterVersionNumber;
+
+//! Project version string for NAMeter.
+FOUNDATION_EXPORT const unsigned char NAMeterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NAMeter/PublicHeader.h>
+
+
